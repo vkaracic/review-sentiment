c03507f971a185a2e571200f6cb107e0a563ce41
Miroslav Dojkic <mdojkic@gmail.com>
2016-10-26 15:00:13 +0200

    Get data from CSV and compare with results

--- monkeylearn/review_sentiment.py
+++ monkeylearn/review_sentiment.py
@@ -12,6 +12,9 @@ import json
 
 import requests
 
+import pandas as pd
+import sys
+
 
 def prepare_parser():
     """ Parse the command line arguments.
@@ -40,7 +43,12 @@ def prepare_parser():
 def prepare_data(filename):
     """ Read the file and convert the lines to a list of strings. """
     with open(filename, 'r') as f:
+        if filename.lower().endswith('.csv'):
+            pd.options.display.max_colwidth = 999
+            data = pd.read_csv(filename, usecols=['review'])
+            return map(str.strip, data.to_string(index=False, header=False, formatters=[str.strip, ]).splitlines())
         return f.read().splitlines()
+
 
 
 def make_request(classifier_id, token, text_list):
@@ -67,7 +75,28 @@ def make_request(classifier_id, token, text_list):
     return response
 
 
+def get_sentiments_from_csv(filename):
+    with open(filename, 'r') as f:
+        pd.options.display.max_colwidth = 999
+        data = pd.read_csv(filename, usecols=['sentiment'])
+        sentiments = data.to_dict(orient='list')['sentiment']
+        return sentiments
+
+
+def get_sentiments_from_results(results):
+    sentiments = map(lambda x: 1 if x == 'Good' else 0, [result[0]['label'] for result in results])
+    return sentiments
+
+
+def compare_results(labeled_sentiments, sentiment_results):
+        matches = [1 if sentiment == result else 0 for sentiment, result in zip(labeled_sentiments, sentiment_results)]
+        return matches
+
+
 def main():
+    reload(sys)
+    sys.setdefaultencoding('utf8')
+
     """ Make requests to aspect and sentiment classifiers and save the results to output file.
     The format of the output file will be:
         review text, list of aspect pairs (probablity, label), sentiment pair (probabilty, label)
@@ -81,13 +110,28 @@ def main():
     aspect_result = json.loads(aspect_response.content)['result']
     sentiment_result = json.loads(sentiment_response.content)['result']
     results = zip(text_list, aspect_result, sentiment_result)
+
     with open(args.output, 'w') as f:
         for result in results:
             aspects = []
             for aspect in result[1]:
                 aspects.append((aspect[0]['probability'], aspect[0]['label']))
             sentiment = (result[2][0]['probability'], result[2][0]['label'])
-            f.write('{},{},{}\n'.format(result[0], aspects, sentiment))
+            f.write('{},{},{}\n'.format(result[0].encode('utf8'), aspects, sentiment))
+
+    if args.input.lower().endswith('.csv'):
+        labeled_sentiments = get_sentiments_from_csv(args.input)
+        labeled_results = get_sentiments_from_results(sentiment_result)
+        matches = compare_results(labeled_sentiments, labeled_results)
+        relative_matching = sum(matches) / float(len(matches))
+
+        with open('matches.txt', 'w') as f:
+            f.write('Matches by row:\n')
+            i = 1
+            for match in matches:
+                f.write('{}. {}\n'.format(i, match))
+                i += 1
+            f.write('\nRelative matching: {}\n'.format(relative_matching))
 
 
 if __name__ == '__main__':
